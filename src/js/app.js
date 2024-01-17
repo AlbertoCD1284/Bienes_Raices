5281b307f50fbbe9ae15b13ecc74fc1061c1ad1c
Alberto <albertocd1284@gmail.com>
2024-01-16 22:20:54 -0500

    Inicio de trabajao con PHP

---
+++ src/js/app.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    eventListenes();
+
+    darkMode();
+});
+
+function darkMode() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // console.log(prefiereDarkMode.matches);
+
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    const botonDarMode = document.querySelector('.dark-mode-boton');
+
+    botonDarMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode')
+    })
+    
+}
+
+
+
+function eventListenes() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector('.navegacion');
+
+    if (navegacion.classList.contains('mostrar')) {
+        navegacion.classList.remove('mostrar');
+    } else {
+        navegacion.classList.add('mostrar')
+    }
+
+    // Lo mismo a lo anterior pero en una linea
+    // navegacion.classList.toggle('mostrar');
+}
